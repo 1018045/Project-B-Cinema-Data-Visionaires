521f52dc9864ce36a5a1a6bec01433e84d8ca62e
1018045 <1018045@hr.nl>
2024-11-13 12:14:57 +0100

    bij inloggen nu ook sterretjes
    laatste toevoeging

--- Project/Presentation/UserLogin.cs
+++ Project/Presentation/UserLogin.cs
@@ -1,3 +1,4 @@
+using System.Security;
 public class UserLogin
 {
     
@@ -9,8 +10,11 @@ public class UserLogin
         Console.WriteLine("Please enter your email address");
         string email = Console.ReadLine();
         Console.WriteLine("Please enter your password");
-        string password = Console.ReadLine();
-        AccountModel acc = AccountsLogic.Logic.CheckLogin(email, password);
+
+        SecureString pass = AccountsLogic.MaskInputstring();  
+        string Password = new System.Net.NetworkCredential(string.Empty, pass).Password; 
+
+        AccountModel acc = AccountsLogic.Logic.CheckLogin(email, Password);
         if (acc != null)
         {
             Console.WriteLine("Welcome back " + acc.FullName);
