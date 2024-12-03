8bc584f6061243873f3fbc95c8401b86c6be1876
1018045 <1018045@hr.nl>
2024-12-03 21:24:14 +0100

    list veranderd

--- Project/DataModels/BillModel.cs
+++ Project/DataModels/BillModel.cs
@@ -1,21 +1,24 @@
+using System.Security.Cryptography.X509Certificates;
+
 public class BillModel 
 {
    public int ID{get;set;}
 
    public bool PaymentCompleted{get;set;}
 
-   public List<Item> FoodOrdered{get;}
+   public List<Item> FoodOrdered { get; set; } = new List<Item>();
 
-   public int Amount{get; set;}
+   public double TotalAmount{get; set;}
 
    public DateTime Paymentdate{get; set;}
 
-   public BillModel(int id, bool paymentCompleted, List<Item> foodOrdered, int amount, DateTime paymentdate)
+   public BillModel(int id, bool paymentCompleted, List<Item> foodOrdered, double amount, DateTime paymentdate)
    {
         ID = id; 
         PaymentCompleted = paymentCompleted; 
         FoodOrdered = foodOrdered; 
         Paymentdate = paymentdate;
+        
    }
 
    public record Item(string name, int amount, double price) {}
