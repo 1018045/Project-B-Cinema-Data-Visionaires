62c72aa5df90cb87f53bc5c2c92befff9d31781b
David ter Borgh <33690947+DavidTerBorgh@users.noreply.github.com>
2024-12-10 19:55:42 +0100

    bugfix1

--- Project/Presentation/Menus.cs
+++ Project/Presentation/Menus.cs
@@ -21,7 +21,7 @@ static class Menus
             Movies.MoviesBrowser,
             Reservation.SelectDate,
             () => Login(),
-            ChooseAccount,
+            () => CreateAccount(LoggedInMenu),
             ApplyForJob.ShowJobMenu,
             () => Environment.Exit(0)
         };
@@ -189,97 +189,7 @@ static class Menus
         MenuHelper.WaitForKey(AdminMenu);
     }
 
-    public static void ChooseAccount()
-    {
-        Console.WriteLine("Email: ");
-        string userEmail;
-        do
-        {
-            Console.WriteLine("Email: ");
-            userEmail = Console.ReadLine();
-        }
-        while(AccountsLogic.VerifyEmail(userEmail) == false || AccountsLogic.CheckForExistingEmail(userEmail) == true);
-
-        Console.WriteLine("Enter your password. It must contain at least 8 characters which consist of 1 capital letter, 1 number, and 1 special character e.g. $,#,% etc.");
-        SecureString pass = AccountsLogic.MaskInputstring();
-        string Password = new System.Net.NetworkCredential(string.Empty, pass).Password; 
-   
-        while (AccountsLogic.VerifyPassword(Password) == false)
-        {
-            Console.WriteLine("Password was not valid. Try again.");
-            pass = AccountsLogic.MaskInputstring();
-            Password = new System.Net.NetworkCredential(string.Empty, pass).Password; 
-        }
-        Console.WriteLine("Confirm your password");
-        
-        string confirmPassword = string.Empty;
-        bool passwordsMatch = false;
-
-        while (!passwordsMatch)
-        {
-            SecureString confirmPass = AccountsLogic.MaskInputstring();
-            confirmPassword = new System.Net.NetworkCredential(string.Empty, confirmPass).Password;
-
-            if (AccountsLogic.VerifyPassword(confirmPassword) && confirmPassword == Password)
-            {
-                passwordsMatch = true;
-            }
-            else
-            {
-                Console.WriteLine("Passwords do not match or are not valid. Try again.");
-            }
-        }
-
-        Console.WriteLine("Password confirmed successfully.");
-
-        Console.WriteLine("Your fullname: ");
-        var fullName = Console.ReadLine();
-        Console.WriteLine("Your age");
-        var userAge = Console.ReadLine();
-        while(AccountsLogic.IsInt(userAge) == false)
-        {
-            Console.WriteLine("Input is not valid. Please enter a number");
-            userAge = Console.ReadLine();
-        }
-
-        AccountsLogic accountsLogic = new();
-        accountsLogic.UpdateList(userEmail, Password, fullName, Convert.ToInt32(userAge));
-
-        Console.WriteLine($"\nSuccessfully created your account, welcome {fullName}!");
-
-        accountsLogic.CheckLogin(userEmail, Password);
-
-        //wait so that it is more clear
-        Thread.Sleep(1500);
-        MenuHelper.WaitForKey(LoggedInMenu);
-    }
-
-    public static void ChooseAccount(Action action)
-    {
-        List<string> options = new List<string>
-        {
-            "Make a reservation",
-            "Show upcoming movie showings",
-            "Show upcoming movie showings on a specific date",
-            "Show your reservations",
-            "Adjust your reservations",
-            "Manage your account",
-            "Log out"
-        };
-        List<Action> actions = new List<Action>
-        {
-            Reservation.Make,
-            () => Showings.ShowUpcoming(),
-            Showings.ShowUpcomingOnDate,
-            () => Reservation.Show(AccountsLogic.CurrentAccount.Id),
-            () => Reservation.Adjust(AccountsLogic.CurrentAccount.Id),
-            AccountPresentation.Menu,
-            AccountsLogic.LogOut
-        };
-        MenuHelper.NewMenu($"Logged in as: {AccountsLogic.CurrentAccount.EmailAddress}", options, actions);
-    }
-
-    public static void CreateAccount()
+    public static void CreateAccount(Action action)
     {
         Console.Clear();
         Console.WriteLine("Enter your information below");
