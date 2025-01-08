dfabf8876943fc07d109da283f3462c723422bc8
Youri Spek <1074481@hr.nl>
2025-01-08 01:19:00 +0100

    yeah done

--- Project/Logic/SeatSelection/SeatSelectionLogic.cs
+++ Project/Logic/SeatSelection/SeatSelectionLogic.cs
@@ -12,22 +12,59 @@ public class SeatSelectionLogic
     private readonly int _seatCount;
     private readonly List<Position> _selectedSeats = [];
     private readonly List<Position> _takenSeats;
+    private readonly int _showingId;
 
     public SeatSelectionLogic(int showingId, int seatCount)
     {
+        _showingId = showingId;
         var room = GetRoomByShowing(showingId);
         _seatCount = seatCount;
         _takenSeats = GetTakenSeats(showingId);
-        GridGenerator = new GridNavigator(room.Width, room.Height);
+        GridGenerator = new GridNavigator(); //todo change
         LayoutGenerator = new LayoutGenerator(room.Id,  showingId, GridGenerator, ref _selectedSeats);
     }
 
     public List<string> StartSeatSelection()
     {
+        var layout = CinemaLayoutParser.GetSeatingLayoutMap(GetRoomByShowing(_showingId).Id);
+        var allowedSeats = CinemaLayoutParser.GenerateNonDecoyPositions(layout);
+        var firstPosAvailable = CinemaLayoutParser.GetFirstNonDecoyPosition(layout);
+
+        GridGenerator.X = firstPosAvailable.X;
+        GridGenerator.Y = firstPosAvailable.Y;
+
         GridGenerator.SelectAction = ActionMethod;
-        GridGenerator.RefreshAction = _ => UpdateSeatingPresentation(LayoutGenerator.BuildSeatingLayoutV2, _seatCount - _selectedSeats.Count);
+        GridGenerator.MovePredicate = futurePos => allowedSeats.Contains(futurePos);
+        GridGenerator.RefreshAction = _ =>
+            UpdateSeatingPresentation(LayoutGenerator.BuildSeatingLayoutV3, layout, _seatCount - _selectedSeats.Count);
+        GridGenerator.ConfirmationAction = nav =>
+        {
+            var seat = layout.SelectMany(row => row).FirstOrDefault(seat => seat.Position.Equals(nav.Cursor));
+            if (seat == null)
+                return false;
+            seat.IsReserved = true;
+            return true;
+        };
+
+        AddTakenSeatsToLayout(layout);
+
         GridGenerator.Start();
+
         return PositionsToStrings(_selectedSeats);
+
+        /*var seatSelector = new SeatSelector();
+        seatSelector.Start(_showingId);
+        return [];*/
+    }
+
+    private void AddTakenSeatsToLayout(List<List<Seat>> seats)
+    {
+        var taken = GetTakenSeats(_showingId);
+        var allSeats = seats.SelectMany(r => r).ToList();
+        foreach (var pos in taken)
+        {
+            allSeats.Find(s => s.Position.Equals(pos))!.IsTaken = true;
+        }
     }
 
     //return true if the WHOLE selection is done
